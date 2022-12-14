0d54b7e9c112e2c8b54e2cb5e208eaedad2a7390
mostafa elsayed <mostafa.elsayed.2002@gmail.com>
2022-12-14 18:00:04 +0200

    MOVE ANY SHAPE WITHOUT RULES IS AVAILABLE NOW....

--- GameCode/Game.asm
+++ GameCode/Game.asm
@@ -7,10 +7,29 @@ PUBLIC imgFilename,MODE,XSTART,YSTART
 .STACK 512
 .DATA
 
+SHAPESTORAGE    DB  'Y','Y' 
+PLACESTORAGEX    DW 0  
+PLACESTORAGEY    DW 0 
+
+
+
   XSTART      DW 0
   YSTART      DW 0
   MODE        DB 2
   COUNT       DB 4
+
+SIZE_XY         DB 75
+rowNo           DB 0
+columnNo        DB 0
+place           DW 0
+
+
+
+
+
+imgFilename DB 'G1', 0
+
+
                 ; 'G','L','W','R',       'G','D','W','H',       'G','L','W','S',       'G','D','W','Q',       'G','L','W','K',       'G','D','W','S',       'G','L','W','H',       'G','D','W','R',
 
                 ; 'G','D','W','P',       'G','L','W','P',       'G','D','W','P',       'G','L','W','P',       'G','D','W','P',       'G','L','W','P',       'G','D','W','P',       'G','L','W','P',
@@ -27,22 +46,25 @@ PUBLIC imgFilename,MODE,XSTART,YSTART
 
                 ; 'G','D','B','R',       'G','L','B','H',       'G','D','B','S',       'G','L','B','Q',       'G','D','B','K',       'G','L','B','S',       'G','D','B','H',       'G','L','B','R',
                  
-STATE0       DW         'G','L','W','R','G','D','W','H','G','L','W','S','G','D','W','Q','G','L','W','K','G','D','W','S','G','L','W','H','G','D','W','R'
-STATE1       DW         'G','D','W','P','G','L','W','P','G','D','W','P','G','L','W','P','G','D','W','P','G','L','W','P','G','D','W','P','G','L','W','P'
-STATE2       DW         'G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X'
-STATE3       DW         'G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X'
-STATE4       DW         'G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X'
-STATE5       DW         'G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X','G','D','X','X','G','L','X','X'
-STATE6       DW         'G','L','B','P','G','D','B','P','G','L','B','P','G','D','B','P','G','L','B','P','G','D','B','P','G','L','B','P','G','D','B','P'
-STATE7       DW         'G','D','B','R','G','L','B','H','G','D','B','S','G','L','B','Q','G','D','B','K','G','L','B','S','G','D','B','H','G','L','B','R'
+STATE0       DB         'G','L','W','R'  ,'G','D','W','H'  ,'G','L','W','S'  ,'G','D','W','Q'  ,'G','L','W','K'  ,'G','D','W','S' ,'G','L','W','H'  ,'G','D','W','R'
+
+STATE1       DB         'G','D','W','P'  ,'G','L','W','P'  ,'G','D','W','P'  ,'G','L','W','P'  ,'G','D','W','P'  ,'G','L','W','P' ,'G','D','W','P'  ,'G','L','W','P'
+
+STATE2       DB         'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X' ,'G','L','X','X'  ,'G','D','X','X'
+
+STATE3       DB         'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X' ,'G','D','X','X'  ,'G','L','X','X'
+
+STATE4       DB         'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X' ,'G','L','X','X'  ,'G','D','X','X'
+
+STATE5       DB         'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X'  ,'G','D','X','X'  ,'G','L','X','X' ,'G','D','X','X'  ,'G','L','X','X'
+
+STATE6       DB         'G','L','B','P'  ,'G','D','B','P'  ,'G','L','B','P'  ,'G','D','B','P'  ,'G','L','B','P'  ,'G','D','B','P' ,'G','L','B','P'  ,'G','D','B','P'
+
+STATE7       DB         'G','D','B','R'  ,'G','L','B','H'  ,'G','D','B','S'  ,'G','L','B','Q'  ,'G','D','B','K'  ,'G','L','B','S' ,'G','D','B','H'  ,'G','L','B','R'
                  
-SIZE_XY         DW 75
-rowNo           DW 0
-columnNo        DW 0
-place           DW 0
 
 
-  imgFilename DB 'G1', 0
+  
 
 .CODE
 MAIN PROC FAR
@@ -63,11 +85,11 @@ MAIN PROC FAR
 
   MYLOOP: 
           MOV  imgFilename[0],'G'
-          MOV  imgFilename[1],'L'
+          MOV  imgFilename[1],'1'
           call PrintGrid
           ADD  YSTART,75
           MOV  imgFilename[0],'G'
-          MOV  imgFilename[1],'D'
+          MOV  imgFilename[1],'2'
           call PrintGrid
           ADD  YSTART,75
           DEC  COUNT
@@ -192,26 +214,31 @@ MAIN PROC FAR
           CALL PRINT
  ;;============================================
 
-  ;;;PRINT Highlight ROW==================================
-          MOV  imgFilename[0],'F'
+  ;;;PRINT Highlight CELL===================================================
+          MOV  imgFilename[0],'F'         
           MOV  imgFilename[1],'R'
-          MOV  XSTART,0
+          MOV  XSTART,0             ;PRINT HIGHLIGHT AT THE FIRST CELL
           MOV  YSTART,0
           MOV MODE,1
           CALL PRINT
-
-
+;;==========================================================================
+;;  THIS PART OF CODE DO 
+;;  WAIT FOR THE KEYBOARD CLICK AND GET IT 
+;;=========================================================================
 	CheckKeyPressed:
         mov AH,00H
         int 16h
-	CMP AH, 48H     ;SCANCODE FOR UP ARROW
+	
+        CMP AH, 48H     ;SCANCODE FOR UP ARROW
         jne not_up
         jmp PRESS_UP
         not_up:
-	CMP AH, 50H     ;SCANCODE FOR DOWN ARROW
+	
+        CMP AH, 50H     ;SCANCODE FOR DOWN ARROW
         jne not_down
         jmp PRESS_DOWN
         not_down:	
+      
         CMP AH, 4BH     ;SCANCODE FOR LEFT ARROW
         jne not_left
         jmp PRESS_LEFT
@@ -219,32 +246,56 @@ MAIN PROC FAR
         CMP AH, 4DH     ;SCANCODE FOR UP ARROW
         jne not_right
         jmp PRESS_RIGHT
-        not_right:	
+        not_right:
+
+        CMP AH,1CH     ;SCANCODE FOR UP ENTER
+        jne not_enter
+        jmp PRESS_ENTER
+        not_enter:
+
+        CMP AH, 01H     ;SCANCODE FOR ESC
+        jne not_esc
+        jmp PRESS_ESC
+        not_esc:
         JMP CheckKeyPressed
 	
-	PRESS_UP:
-        ;CALL GetPlace   ;set the 'place' value
-        ;CALL PrintInState       ;removing the highlight from current cell
+;;==========================================================================
+;;  THIS PART OF CODE DO 
+;;  1-MOVE HIGHLIGHT OF FIRST PLAYER
+;;=========================================================================
+        PRESS_UP:
+        MOV BX,YSTART
+        SUB BX,75 
+        CMP BX,0
+        JL CheckKeyPressed
+        CALL PrintInState          ;removing the highlight from current cell
         MOV  imgFilename[0],'F' 
         MOV  imgFilename[1],'R'
         MOV MODE,1
-	SUB YSTART,75           ;printing the highlight in the new cell
-        CALL PRINT
+        SUB YSTART,75  
+	CALL PRINT               ;printing the highlight in the new cell
         JMP CheckKeyPressed
 
         PRESS_DOWN:
-        ;CALL GetPlace
-        ;CALL PrintInState
+        MOV BX,YSTART
+        ADD BX,75 
+        CMP BX,525
+        JA CheckKeyPressed
+        CALL PrintInState
         MOV  imgFilename[0],'F'
         MOV  imgFilename[1],'R'
         MOV MODE,1
 	ADD YSTART,75
         CALL PRINT
+        JMPGO:
         JMP CheckKeyPressed
 
 	PRESS_LEFT:
-        ;CALL GetPlace
-        ;CALL PrintInState
+        MOV BX,XSTART
+        SUB BX,75 
+        CMP BX,0
+        JL JMPGO
+        CALL PrintInState
         MOV  imgFilename[0],'F'
         MOV  imgFilename[1],'R'
         MOV MODE,1
@@ -252,20 +303,117 @@ MAIN PROC FAR
         CALL PRINT
         JMP CheckKeyPressed
 	
-	PRESS_RIGHT:
-        ;CALL GetPlace
-        ;CALL PrintInState
+	PRESS_RIGHT: 
+        MOV BX,XSTART
+        ADD BX,75 
+        CMP BX,525
+        JA JMPGO
+        CALL PrintInState
         MOV  imgFilename[0],'F'
         MOV  imgFilename[1],'R'
         MOV MODE,1
-	ADD xSTART,75
+	ADD XSTART,75
         CALL PRINT
+        here:
         JMP CheckKeyPressed
 
-        ENDKEYPRESSED:
+;;==========================================================================
+;;  THIS PART OF CODE DO 
+;;  1-WHEN FIRST PLAYER PRESSED ENTER ON A CELL
+;;  2-THEN CHECK IF THE "SHAPESTORAGE" IS EMPTY OR NOT 
+;;     3-IF IT IS EMPTY(START WITH 'Y') WE STORE ITS SHAPE IN "SHAPESTORAGE"
+;;     4-AND STORE ITS X,Y IN "PLACESTORAGEX" ,"PLACESTORAGEY"
+;;  5-IF IT IS NOT EMPTY GET THE STORED SHAPE AND ITS X,Y
+;;     6- DELETE THE OLD SHAPE
+;;     7- PRINT THE OLD SHAPE IN CURRENT X,Y
+;;=========================================================================
+
+       PRESS_ENTER:
+       CMP SHAPESTORAGE[0],'Y'    ;CHECK IF IS EMPTY
+       JNE NOTEMPTY
+
+       ;;EMPTY --> GET ITS PLACE WHICH IS WANTED TO MOVE 
+       CALL GETPLACE       
+       MOV BX ,place 
+      ;;THEN  GET THE SHAPE WHICH IS WANTED TO MOVE
+      ;; AND STORE  IT IN  "SHAPESTORAGE"      
+       MOV AL,STATE0[BX+2]
+       CMP AL,'X'          ;;THIS CHECK IF IT IS EMPY CELL AND PLAYER WANT TO MOVE IF WHICH IS FORBIEDDEN                                                  
+       JE here                                                                                   
+       MOV SHAPESTORAGE[0],AL                           
+       MOV AL,STATE0[BX+3]
+       MOV SHAPESTORAGE[1],AL
+
+      ;;THEN  GET THE X,Y
+      ;; AND STORE  IT IN  "PLACESTORAGEX" ,PLACESTORAGEY
+       MOV AX,XSTART                 
+       MOV PLACESTORAGEX,AX
+       MOV AX,YSTART                         
+       MOV PLACESTORAGEY,AX                    
+       JMP CheckKeyPressed
+ 
+    ;;IF IT IS NOT EMPTY WE DO THE FOLLOWING:
+       NOTEMPTY:
+       MOV DX,XSTART    
+       MOV CX,YSTART                        
+       PUSH DX                 
+       PUSH CX                        ;;GET OLD PLACE
+       MOV AX,PLACESTORAGEX  
+       MOV XSTART,AX                         
+       MOV AX ,PLACESTORAGEY
+       MOV YSTART,AX
+
+       CALL GETPLACE
+       MOV BX,place                  ;;WE DELETE THE OLD SHAPE
+       MOV AL,state0 [BX]                 
+       MOV  imgFilename[0], AL 
+       MOV AL, state0[BX+1]          ;printing the background OF THE OLD PLACE SO THE OLD SHAPE ID DISAPPERED
+       MOV  imgFilename[1], AL
+
+       MOV STATE0[BX+2],'X'       ;;UPDATE THE STATE ARRAY
+       MOV STATE0[BX+3],'X'
+       MOV MODE,2
+       CALL PRINT
+       
+       ;; HERE WE PRINT ON THE NEW CELL
+       POP CX 
+       POP DX
+       MOV XSTART,DX
+       MOV YSTART,CX
+       CALL GETPLACE
+       MOV BX,place                                       
+       PUSH BX
+       MOV AL,state0 [BX]                 
+       MOV  imgFilename[0], AL 
+       MOV AH, state0[BX+1]          ;printing the background  OF THE NEW CELL  TO DELETE THE ANIMY SHAPE IF EXSITS          
+       MOV  imgFilename[1], AH  
+       MOV MODE,2
+       CALL PRINT
+   
+       POP BX 
+       MOV AL,SHAPESTORAGE[0]
+       MOV  imgFilename[0], AL            ;;GET OLD SHAPE AND PRINT IT ON THE NEW CELL
+       MOV STATE0[BX+2],AL                                        
+       MOV  AL,SHAPESTORAGE[1]
+       MOV  imgFilename[1], AL
+       MOV  STATE0[BX+3],AL 
+       MOV MODE ,0
+       CALL PRINT
+        
+        MOV SHAPESTORAGE[0],'Y'            ;;RETURN THE SHAPE STORAGE EMPTY AGAIN
+        MOV SHAPESTORAGE[1],'Y'
+      JMP CheckKeyPressed
+;;==========================================================================
+;;=========================================================================
 
 
 
+
+
+       
+
+
+ PRESS_ESC:
   ; Press any key to exit
           MOV  AH , 0
           INT  16h
@@ -281,48 +429,93 @@ MAIN PROC FAR
 
 MAIN ENDP
 
-;GetPlace PROC
-;        MOV AX, XSTART     
-;        DIV SIZE_XY         
-;        MOV rowNo,AX         ;rowNo = XSTART/SIZE_XY
-;
-;        MOV AX, YSTART     
-;        DIV SIZE_XY         
-;        MOV columnNo,AX      ;columnNo = YSTART/SIZE_XY
-;               
-;        MOV BX,8
-;        MUL BX               ;AX = columnNo * 8                    
-;        ADD AX,rowNo         ;AX = (columnNo*8) + rowNo
-;        
-;        MOV BX,4     
-;        MUL BX               ;AX = [ (columnNo*8) + rowNo ] *4
-;
-;        Mov place,AX
 
-;GetPlace ENDP
+PrintInState PROC       ;prints the square in 'place'
 
-;PrintInState PROC       ;prints the square in 'place'
-;
-;        MOV BX,offset state0    ;gets a pointer to first element in state0
-;        ADD BX,place            ;adds the place to the pointer to get the values in place
-;        MOV AX, [BX]            ;moves the first 2 bytes into ax
-;
-;        MOV  imgFilename[0], AL
-;        MOV  imgFilename[1], AH
-;        MOV MODE,2
-;        CALL PRINT              ;printing the background
-;
-;        MOV BX,offset state0    
-;        ADD BX,place
-;        ADD BX,2                ;gets the second 2 bytes after the state0+place 
-;        MOV AX, [BX]
-;
-;        MOV  imgFilename[0], AL
-;        MOV  imgFilename[1], AH
-;        MOV MODE,0
-;        CALL PRINT              ;printing the piece
-;
-;PrintInState ENDP
+                ;525
+        MOV AX, XSTART      
+        MOV BL,SIZE_XY         ;rowNo = XSTART/SIZE_XY
+        DIV BL 
+        MOV rowNo,AL        
+                          
+        MOV AX, YSTART                               
+        DIV SIZE_XY          ;columnNo = YSTART/SIZE_XY
+        MOV columnNo,AL       
+        MOV AH,0
+           
+        MOV BL,8
+        MUL BL               ;AX = columnNo * 8                    
+
+        ADD AL,rowNo        ;AX = (columnNo*8) + rowNo   
+        MOV BL,4     
+        MUL BL               ;AX = [ (columnNo*8) + rowNo ] *4
+
+        Mov place,AX
+       
+                              
+        MOV BX,place            
+        MOV AL,state0 [BX]                 
+        MOV  imgFilename[0], AL 
+        MOV AH, state0[BX+1]          ;printing the background
+        MOV  imgFilename[1], AH  
+        MOV MODE,2
+        CALL PRINT
+       
+        MOV BX,place                               
+        MOV AL,state0 [BX+2]     
+        MOV  imgFilename[0], AL       ;printing the PICE                   
+        MOV AH, state0[BX+3]  
+        MOV  imgFilename[1], AH 
+        CMP AL,'X'
+        JE EXIT0 
+        CMP AL,'W'
+         JNE BLACK
+         MOV MODE,0
+         JMP GOHERE
+         BLACK:
+         MOV MODE,1
+    
+        GOHERE:
+        CALL PRINT
+        EXIT0:
+        ret
+PrintInState ENDP
+
+
+
+GETPLACE PROC  
+
+             ;525
+        MOV AX, XSTART      
+        MOV BL,SIZE_XY         ;rowNo = XSTART/SIZE_XY
+        DIV BL 
+        MOV rowNo,AL        
+                          
+        MOV AX, YSTART                               
+        DIV SIZE_XY          ;columnNo = YSTART/SIZE_XY
+        MOV columnNo,AL       
+        MOV AH,0
+           
+        MOV BL,8
+        MUL BL               ;AX = columnNo * 8                    
+
+        ADD AL,rowNo        ;AX = (columnNo*8) + rowNo   
+        MOV BL,4     
+        MUL BL               ;AX = [ (columnNo*8) + rowNo ] *4
+
+        Mov place,AX
+
+
+        ret
+GETPLACE ENDP
+
+
+
+
+
+
+
+
 
 END MAIN    
 
